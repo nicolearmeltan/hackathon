58e82fe351c396933574dc30acd9c5748f23707f
Delfin <bryll.joey.g.delfin@accenture.com>
2018-10-23 17:50:52 +0800

    finished common ailments report

--- hackathon-AI-HRDashboard-Backend/routes.js
+++ hackathon-AI-HRDashboard-Backend/routes.js
@@ -20,7 +20,7 @@ router.post('/createGraphs', function (req, res) {
 });
 router.get('/LeaveReport', async function (req, res) {
   console.log(chalk.blue('proceeding to db'));
-  var something  = await leaveDB.aggregate([
+  var something = await leaveDB.aggregate([
     {
       $group: {
         _id: { month: "$Month of absence" },
@@ -28,13 +28,13 @@ router.get('/LeaveReport', async function (req, res) {
       }
     }
   ]);
-  for(var i in something){
-    if(something[i]._id.month === 0 ||something[i]._id.month === ""){
-      something.splice(i,1);
+  for (var i in something) {
+    if (something[i]._id.month === 0 || something[i]._id.month === "") {
+      something.splice(i, 1);
     }
   }
-  something.sort(function(a,b){
-    return a._id.month - b._id.month 
+  something.sort(function (a, b) {
+    return a._id.month - b._id.month
   });
   console.log(chalk.magentaBright('sending front end json object', something.length));
   res.send(something);
@@ -44,7 +44,7 @@ router.get('/PerformanceReport', function (req, res) {
   console.log(chalk.magentaBright('sending front end json object'));
   res.send({ "called": "PeformanceReport" });
 });
-router.get('/QOLReport', function (req, res) { 
+router.get('/QOLReport', function (req, res) {
   console.log(chalk.green('Called QOLReport calling Azure endpoint .....'));
   console.log(chalk.magentaBright('sending front end json object'));
   res.send({ "called": "QOLReport" });
@@ -54,10 +54,80 @@ router.get('/SickLeaveAnalytics/DailySickLeavePrediction', function (req, res) {
   console.log(chalk.magentaBright('sending front end json object'));
   res.send({ "called": "DailySickLeavePrediction" });
 });
-router.get('/SickLeaveAnalytics/AilmentsReport', function (req, res) {
+router.get('/SickLeaveAnalytics/AilmentsReport', async function (req, res) {
   console.log(chalk.green('Called AilmentsReport calling Azure endpoint .....'));
-  console.log(chalk.magentaBright('sending front end json object'));
-  res.send({ "called": "AilmentsReport" });
+  let url = "https://ussouthcentral.services.azureml.net/workspaces/5d99b1e5dab04ea6a85f0a01e57d5481/services/ce4079d90f774b1f90d0482ab8130ffb/execute?api-version=2.0&format=swagger";
+  let MonthlyAilmentReport = [];
+  let token = "Bearer qWy40BmquZLGTWhnw0im03hA4vvbyOed0dLVgQWhAhj5eRC37l/8WE3QCtrs3Sau42vLg6DGdaxt620i6O+EOQ==";
+  let data = {
+    "Inputs": {
+      "input1":
+        [
+          {
+            'ID': "1",
+            'Reason for absence': "1",
+            'Month of absence': "1",
+            'Day of the week': "1",
+            'Seasons': "1",
+            'Transportation expense': "1",
+            'Distance from Residence to Work': "1",
+            'Service time': "1",
+            'Age': "1",
+            'Work load Average/day': "1",
+            'Hit target': "1",
+            'Disciplinary failure': "1",
+            'Education': "1",
+            'Son': "1",
+            'Social drinker': "1",
+            'Social smoker': "1",
+            'Pet': "1",
+            'Weight': "1",
+            'Height': "1",
+            'Body mass index': "1",
+            'Absenteeism time in hours': "1",
+          }
+        ],
+    },
+    "GlobalParameters": {
+    }
+  };
+  var test = new Promise(async function (resolve, rej) {
+    console.log('logging Pramis')
+    for (var i = 1; i < 13; i++) {
+      data["Inputs"]["input1"][0]['Month of absence'] = i;
+      var promiseRequest = new Promise(function (resolved, rejected) {
+        request.post({
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': token
+          },
+          url: url,
+          body: JSON.stringify(data)
+        }, function (err, response, body) {
+          if (err) {
+            throw Error(err);
+          } else {
+            body = JSON.parse(body);
+            let res = {}
+            res.Month = body["Results"]["output1"][0]["Month of absence"];
+            res.Ailment = body["Results"]["output1"][0]["Scored Labels"];
+            resolved(res);
+            console.log('After a call')
+          }
+        });
+      })
+      await promiseRequest.then(function (promise) {
+        console.log('after all calls')
+        MonthlyAilmentReport.push(promise);
+        MonthlyAilmentReport.sort(function (a, b) {
+          return a.Month - b.Month
+        });
+        console.log('should resolve')
+        resolve(MonthlyAilmentReport);
+      })
+    }
+    res.send(MonthlyAilmentReport);
+  });
 });
 router.get('/SickLeaveAnalytics/WorkStressReport', function (req, res) {
   console.log(chalk.green('Called WorkStressReport calling Azure endpoint .....'));
