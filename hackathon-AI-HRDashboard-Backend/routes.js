360ca2047a6d3f0c8a8073c39f2233bd355b70b8
Delfin <bryll.joey.g.delfin@accenture.com>
2018-10-23 19:54:37 +0800

    added workstress analytics

--- hackathon-AI-HRDashboard-Backend/routes.js
+++ hackathon-AI-HRDashboard-Backend/routes.js
@@ -141,7 +141,101 @@ router.get('/SickLeaveAnalytics/AilmentsReport', async function (req, res) {
 });
 router.get('/SickLeaveAnalytics/WorkStressReport', function (req, res) {
   console.log(chalk.green('Called WorkStressReport calling Azure endpoint .....'));
-  console.log(chalk.magentaBright('sending front end json object'));
-  res.send({ "called": "WorkStressReport" });
+  let url = "https://ussouthcentral.services.azureml.net/workspaces/5d99b1e5dab04ea6a85f0a01e57d5481/services/563023e45ecf41adb28459648689003f/execute?api-version=2.0&format=swagger";
+  let MonthlyAilmentReport = [];
+  let ranking = [];
+  let token = "Bearer 4JlGhSh8zuqECfaKuxz+f/ioJikyGrHw4D2HiUpwjGT1yqrdO8rhAILFlNEcNODWsEqo5RxUZSvjDlbHvNznog==";
+  let data = {
+    "Inputs": {
+            "input1":
+            [
+                {
+                        'Age': "1",   
+                        'Attrition': "1",   
+                        'BusinessTravel': "1",   
+                        'DailyRate': "1",   
+                        'Department': "1",   
+                        'DistanceFromHome': "1",   
+                        'Education': "1",   
+                        'EducationField': "1",   
+                        'EmployeeCount': "1",   
+                        'EmployeeNumber': "1",   
+                        'EnvironmentSatisfaction': "1",   
+                        'Gender': "1",   
+                        'HourlyRate': "1",   
+                        'JobInvolvement': "1",   
+                        'JobLevel': "1",   
+                        'JobRole': "Test",   
+                        'JobSatisfaction': "1",   
+                        'MaritalStatus': "1",   
+                        'MonthlyIncome': "1",   
+                        'MonthlyRate': "1",   
+                        'NumCompaniesWorked': "1",   
+                        'Over18': "1",   
+                        'OverTime': "1",   
+                        'PercentSalaryHike': "1",   
+                        'PerformanceRating': "1",   
+                        'RelationshipSatisfaction': "1",   
+                        'StandardHours': "1",   
+                        'StockOptionLevel': "1",   
+                        'TotalWorkingYears': "1",   
+                        'TrainingTimesLastYear': "1",   
+                        'WorkLifeBalance': "1",   
+                        'YearsAtCompany': "1",   
+                        'YearsInCurrentRole': "1",   
+                        'YearsSinceLastPromotion': "1",   
+                        'YearsWithCurrManager': "1",   
+                        'Month of absence': "1",   
+                }
+            ],
+    },
+"GlobalParameters":  {
+}
+};
+  var test = new Promise(async function (resolve, rej) {
+    console.log('logging Pramis')
+    for (var i = 1; i < 13; i++) {
+      data["Inputs"]["input1"][0]['JobRole'] = "Test"+i;
+      data["Inputs"]["input1"][0]['Month of absence'] = i;
+      var promiseRequest = new Promise(function (resolved, rejected) {
+        request.post({
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': token
+          },
+          url: url,
+          body: JSON.stringify(data)
+        }, function (err, response, body) {
+          if (err) {
+            throw Error(err);
+          } else {
+            body = JSON.parse(body);
+            let res = {}
+            res.num = body["Results"]["output1"][0]["Month of absence"];
+            res.Month = ntdmn.toMonth(body["Results"]["output1"][0]["Month of absence"]);
+            res.JobRole = body["Results"]["output1"][0]["Scored Labels"];
+            resolved(res);
+            console.log('After a call')
+          }
+        });
+      })
+      await promiseRequest.then(function (promise) {
+        ranking.push(promise.JobRole);
+        console.log(ranking)
+        MonthlyAilmentReport.push(promise);
+        MonthlyAilmentReport.sort(function (a, b) {
+          return a.num - b.num
+        });
+        console.log('should resolve')
+        resolve(MonthlyAilmentReport);
+      })
+    }
+    let test = count(ranking);
+    let sagot = {
+      ranking: test,
+      MonthlyAilmentReport:MonthlyAilmentReport
+    };
+    res.send(sagot);
+  });
 });
 module.exports = router
